6132cc5f8715e59e00d73ed02029983477475a75
Aleksandra <a.walczak1984@gmail.com>
2023-07-26 19:54:43 +0200

    changes in index.js added prvider, persistGate

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
-
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
-
-// import React from 'react';
-// import { createRoot } from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 // import store from './redux/store';
-// import App from './components/App';
+import { store, persistor } from './redux/store';
+import App from './components/App';
+// import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // createRoot(document.getElementById('root')).render(
 //   <Provider store={store}>
-//     <Router>
-//       {' '}
-//       {/* Owiń komponent App w Router */}
+//     <BrowserRouter>
 //       <App />
-//     </Router>
+//     </BrowserRouter>
 //   </Provider>
 // );
