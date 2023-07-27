0548992be3f330e400068bf3fd43195454019afa
Aleksandra <a.walczak1984@gmail.com>
2023-07-27 16:21:59 +0200

    changes in index.js,store,js added middleware etc

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import App from './components/App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
+
 // import { createRoot } from 'react-dom/client';
 // import store from './redux/store';
-// import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <App />
+//     </PersistGate>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
 // createRoot(document.getElementById('root')).render(
 //   <Provider store={store}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,64 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authSlice from './auth/authSlice';
-import contactsSlice from './contacts/contactsSlice';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import contactsReducer from './contacts/contactsSlice';
+import authReducer from './auth/authSlice';
+// import authSlice from './auth/authSlice';
+// import contactsSlice from './contacts/contactsSlice';
 
-const store = configureStore({
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth', // klucz, pod którym będą przechowywane dane w magazynie
+  storage, //  magazyn do przechowywania danych (np. Local Storage)
+  whitelist: ['token'], // lista slice'ów o nazwie token przechowywana w magazynie
+};
+
+export const store = configureStore({
   reducer: {
-    contacts: contactsSlice.reducer,
-    auth: authSlice.reducer,
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
   },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
 
-export default store;
+export const persistor = persistStore(store);
+
+// const persistedReducer = persistReducer(
+//   authPersistConfig,
+//   combineReducers({
+//     auth: authReducer,
+//     contacts: contactsReducer,
+//   })
+// );
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware,
+//   devTools: process.env.NODE_ENV === 'development',
+// });
+
+// const store = configureStore({
+//   reducer: {
+//     contacts: contactsSlice.reducer,
+//     auth: authSlice.reducer,
+//   },
+// });
+
+// export default store;
