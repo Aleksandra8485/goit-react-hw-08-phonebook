fddc7d000994120bb6ac35ba68dbe9d28e559bfd
Aleksandra <a.walczak1984@gmail.com>
2023-07-27 14:44:26 +0200

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-// import store from './redux/store';
 import { store, persistor } from './redux/store';
 import App from './components/App';
+// import { createRoot } from 'react-dom/client';
+// import store from './redux/store';
 // import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
