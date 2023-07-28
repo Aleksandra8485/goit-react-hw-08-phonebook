59a19229e52aeb9d20b4a0e637c1f4af5fc781cd
Aleksandra <a.walczak1984@gmail.com>
2023-07-28 19:24:17 +0200

    changes in used menu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,37 +1,54 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
-const UserMenu = () => {
-  // stan do przechowywania informacji o zalogowanym użytkowniku
-  const [userEmail, setUserEmail] = useState('mango@mail.com');
+import { logOut } from 'redux/auth/operations';
+import { useAuth } from 'hooks/useAuth';
 
-  // pobieranie dispatch z Redux, aby móc wywołać akcję wylogowania użytkownika
+export const UserMenu = () => {
   const dispatch = useDispatch();
+  const { user } = useAuth();
 
-  // pobieranie navigate z react-router-dom, aby przekierować użytkownika po wylogowaniu
-  const navigate = useNavigate(); // zmiana użycia useHistory na useNavigate- błędy w konsoli
-
-  // funkcja obsługująca wylogowanie użytkownika
-  const handleLogout = () => {
-    // tu można wysłać żądanie HTTP do backendu, aby wylogować użytkownika
-    // zerowanie poczty użytkownika w stanie
-    setUserEmail('');
-
-    // wywowałanie akcji wylogowania
-    dispatch({ type: 'LOGOUT' });
-
-    // przekierowanie użytkownika na stronę logowania po wylogowaniu
-    navigate('/login'); // zmiana użycie history.push na navigate- błędy w  konsoli
-  };
-
-  // komponent UserMenu wyświetla pocztę użytkownika i przycisk wylogowania
   return (
     <div>
-      <p>{userEmail}</p>
-      <button onClick={handleLogout}>Logout</button>
+      <p>Welcome, {user.name}</p>
+      <button type="button" onClick={() => dispatch(logOut())}>
+        Logout
+      </button>
     </div>
   );
 };
 
-export default UserMenu;
+// const UserMenu = () => {
+//   // stan do przechowywania informacji o zalogowanym użytkowniku
+//   const [userEmail, setUserEmail] = useState('mango@mail.com');
+
+//   // pobieranie dispatch z Redux, aby móc wywołać akcję wylogowania użytkownika
+//   const dispatch = useDispatch();
+
+//   // pobieranie navigate z react-router-dom, aby przekierować użytkownika po wylogowaniu
+//   const navigate = useNavigate(); // zmiana użycia useHistory na useNavigate- błędy w konsoli
+
+//   // funkcja obsługująca wylogowanie użytkownika
+//   const handleLogout = () => {
+//     // tu można wysłać żądanie HTTP do backendu, aby wylogować użytkownika
+//     // zerowanie poczty użytkownika w stanie
+//     setUserEmail('');
+
+//     // wywowałanie akcji wylogowania
+//     dispatch({ type: 'LOGOUT' });
+
+//     // przekierowanie użytkownika na stronę logowania po wylogowaniu
+//     navigate('/login'); // zmiana użycie history.push na navigate- błędy w  konsoli
+//   };
+
+//   // komponent UserMenu wyświetla pocztę użytkownika i przycisk wylogowania
+//   return (
+//     <div>
+//       <p>{userEmail}</p>
+//       <button onClick={handleLogout}>Logout</button>
+//     </div>
+//   );
+// };
+
+// export default UserMenu;
